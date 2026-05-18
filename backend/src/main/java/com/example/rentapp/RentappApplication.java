476c384b4f673493f603785f85559f6b998c26ed
patrickblg <blaga.patrick@yahoo.com>
2026-05-18 17:14:49 +0300

    Add files via upload

---
+++ backend/src/main/java/com/example/rentapp/RentappApplication.java
@@ -0,0 +1,13 @@
+package com.example.rentapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//start app
+@SpringBootApplication
+public class RentappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentappApplication.class, args);
+	}
+
+}
